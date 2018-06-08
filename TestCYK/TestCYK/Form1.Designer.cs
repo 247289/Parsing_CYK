d896c2b7edeaabfbfd338e4e7c5d2506b5cbfa61
loc nguyen <nguyenlocitpro@outlook.com>
2018-06-08 21:51:31 +0700

    update
    v1-8.6.18

---
+++ TestCYK/TestCYK/Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace TestCYK
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pnl_Main = new System.Windows.Forms.Panel();
+            this.lbl_rule = new System.Windows.Forms.Label();
+            this.lbl_list = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btn_test = new System.Windows.Forms.Button();
+            this.txt_input = new System.Windows.Forms.TextBox();
+            this.lbl_input = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblTich = new System.Windows.Forms.Label();
+            this.txtH_btn = new System.Windows.Forms.TextBox();
+            this.txtW_btn = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnl_Main
+            // 
+            this.pnl_Main.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pnl_Main.Location = new System.Drawing.Point(125, 53);
+            this.pnl_Main.Name = "pnl_Main";
+            this.pnl_Main.Size = new System.Drawing.Size(736, 414);
+            this.pnl_Main.TabIndex = 0;
+            // 
+            // lbl_rule
+            // 
+            this.lbl_rule.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbl_rule.AutoSize = true;
+            this.lbl_rule.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_rule.Location = new System.Drawing.Point(3, 6);
+            this.lbl_rule.Name = "lbl_rule";
+            this.lbl_rule.Size = new System.Drawing.Size(39, 17);
+            this.lbl_rule.TabIndex = 5;
+            this.lbl_rule.Text = "Rule";
+            // 
+            // lbl_list
+            // 
+            this.lbl_list.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbl_list.AutoSize = true;
+            this.lbl_list.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.lbl_list.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_list.ForeColor = System.Drawing.Color.Lime;
+            this.lbl_list.Location = new System.Drawing.Point(3, 40);
+            this.lbl_list.Name = "lbl_list";
+            this.lbl_list.Size = new System.Drawing.Size(12, 17);
+            this.lbl_list.TabIndex = 6;
+            this.lbl_list.Text = " ";
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lbl_rule);
+            this.panel1.Controls.Add(this.lbl_list);
+            this.panel1.Location = new System.Drawing.Point(0, 53);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(123, 179);
+            this.panel1.TabIndex = 7;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.btn_test);
+            this.panel2.Controls.Add(this.txt_input);
+            this.panel2.Controls.Add(this.lbl_input);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(861, 49);
+            this.panel2.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(475, 11);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(83, 26);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Refresh btn";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn_test
+            // 
+            this.btn_test.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_test.Location = new System.Drawing.Point(394, 11);
+            this.btn_test.Name = "btn_test";
+            this.btn_test.Size = new System.Drawing.Size(75, 26);
+            this.btn_test.TabIndex = 7;
+            this.btn_test.Text = "Test";
+            this.btn_test.UseVisualStyleBackColor = true;
+            this.btn_test.Click += new System.EventHandler(this.btn_test_Click);
+            // 
+            // txt_input
+            // 
+            this.txt_input.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_input.Location = new System.Drawing.Point(73, 12);
+            this.txt_input.Name = "txt_input";
+            this.txt_input.Size = new System.Drawing.Size(306, 25);
+            this.txt_input.TabIndex = 6;
+            // 
+            // lbl_input
+            // 
+            this.lbl_input.AutoSize = true;
+            this.lbl_input.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_input.Location = new System.Drawing.Point(15, 15);
+            this.lbl_input.Name = "lbl_input";
+            this.lbl_input.Size = new System.Drawing.Size(43, 17);
+            this.lbl_input.TabIndex = 5;
+            this.lbl_input.Text = "Input";
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Controls.Add(this.lblTich);
+            this.panel3.Controls.Add(this.txtH_btn);
+            this.panel3.Controls.Add(this.txtW_btn);
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Location = new System.Drawing.Point(0, 238);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(123, 229);
+            this.panel3.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.label2.Location = new System.Drawing.Point(0, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 136);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Hướng Dẫn\n\n Nhập text cần kiểm tra vào ô input sau đó kích test để chạy chương tr" +
+    "ình\n VD: aaabbb\n";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // lblTich
+            // 
+            this.lblTich.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblTich.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.lblTich.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTich.ForeColor = System.Drawing.Color.Black;
+            this.lblTich.Location = new System.Drawing.Point(44, 57);
+            this.lblTich.Name = "lblTich";
+            this.lblTich.Size = new System.Drawing.Size(23, 20);
+            this.lblTich.TabIndex = 11;
+            this.lblTich.Text = " X";
+            // 
+            // txtH_btn
+            // 
+            this.txtH_btn.Location = new System.Drawing.Point(67, 57);
+            this.txtH_btn.Multiline = true;
+            this.txtH_btn.Name = "txtH_btn";
+            this.txtH_btn.Size = new System.Drawing.Size(23, 20);
+            this.txtH_btn.TabIndex = 10;
+            this.txtH_btn.Text = "50";
+            // 
+            // txtW_btn
+            // 
+            this.txtW_btn.Location = new System.Drawing.Point(21, 57);
+            this.txtW_btn.Multiline = true;
+            this.txtW_btn.Name = "txtW_btn";
+            this.txtW_btn.Size = new System.Drawing.Size(23, 20);
+            this.txtW_btn.TabIndex = 9;
+            this.txtW_btn.Text = "60";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 26);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Chọn kích thước\n button";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btn_test;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(861, 470);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pnl_Main);
+            this.Name = "Form1";
+            this.Text = "Phần mềm mô phỏng thuật toán CYK";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnl_Main;
+        private System.Windows.Forms.Label lbl_rule;
+        private System.Windows.Forms.Label lbl_list;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btn_test;
+        private System.Windows.Forms.TextBox txt_input;
+        private System.Windows.Forms.Label lbl_input;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label lblTich;
+        private System.Windows.Forms.TextBox txtH_btn;
+        private System.Windows.Forms.TextBox txtW_btn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ TestCYK/TestCYK/Form1.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestCYK
+{
+    public partial class Form1 : Form
+    {
+        public void Createbutton(int witdh, int height, string w, string[,] table, int end)
+        {
+            int n = w.Length;
+            Button oldbtn = new Button() { Width = 0, Location = new Point( 0 , n* height) };
+            int tmp;
+           
+            for (int i = 0; i <= end; i++)
+            {
+                for (int j = 0; j <= n + 1; j++)
+                {
+                    tmp = j;
+                    Button btn = new Button()
+                    {
+                        Width = witdh,
+                        Height = height,
+                        Location = new Point(oldbtn.Width + oldbtn.Location.X, oldbtn.Location.Y),
+                        BackColor = Color.White,
+                        TextAlign = System.Drawing.ContentAlignment.MiddleCenter,
+                        Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0))),
+                    };
+
+                    pnl_Main.Controls.Add(btn);
+                    if (tmp == n + 1) tmp = n;
+                    btn.Text = table[tmp, i];
+                    if (i == 0 || j == 0)
+                    {
+                        btn.BackColor = Color.LightBlue;
+                    }
+                    oldbtn = btn;
+                    //Thread.Sleep(5000);
+                }
+                oldbtn.Location = new Point(0, oldbtn.Location.Y - height);
+                oldbtn.Width = 0;
+                oldbtn.Height = 0;
+
+            }
+        }
+
+        class Rule
+        {
+            public char left;
+            public string right;
+            public Rule(char l, string r)
+            {
+                left = l; right = r;
+            }
+            public string ToFineString()
+            {
+                string s = left + " -->";
+                for (int i = 0; i < right.Length; i++)
+                    s += " " + right[i];
+                return s;
+            }
+        }
+        class PTCYK
+        {
+            public List<Rule> rules = new List<Rule>();
+            string[,] table = null;
+            string w = null;
+            int n = 0;
+
+            // thêm luật left --> right vào tập luật
+            public void AddRule(char left, string right)
+            {
+                rules.Add(new Rule(left, right));
+            }
+
+            public List<Rule> PrintAllRules()
+            {
+                return rules;
+                //Console.WriteLine("<bo luat van pham>");
+                //foreach (Rule r in rules)
+                //    Console.WriteLine("  " + r.ToFineString());
+                //Console.WriteLine();
+            }
+            /// <summary>
+            /// 
+            /// </summary>
+            /// <param name="x"></param>
+            void InitData(string x)
+            {
+                w = x;
+                n = x.Length;
+                table = new string[n + 1, n + 1];
+                for (int i = 1; i <= n; i++)
+                    for (int j = 1; j <= n; j++) table[i, j] = "";
+            }
+            /// <summary>
+            /// 
+            /// </summary>
+            void FirstProcess()
+            {
+                for (int i = 1; i <= n; i++)
+                    foreach (Rule r in rules)
+                        if (r.right.Length == 1)
+                            if (w[i - 1] == r.right[0]) table[i, 1] += r.left;
+            }
+            /// <summary>
+            /// 
+            /// </summary>
+            /// <param name="x"></param>
+            /// <param name="w"></param>
+            /// <returns></returns>
+            bool IsIn(char x, string w)
+            {
+                return w.IndexOf(x) != -1;
+            }
+            /// <summary>
+            /// 
+            /// </summary>
+            /// <param name="j"></param>
+            /// <param name="i"></param>
+            /// <param name="r"></param>
+            /// <returns></returns>
+            bool CanGen(int j, int i, Rule r)
+            {
+                for (int k = 1; k <= j - 1; k++)
+                    if (IsIn(r.right[0], table[i, k]))
+                        if (IsIn(r.right[1], table[i + k, j - k]))
+                            return true;
+                return false;
+            }
+
+            void GenTable()
+            {
+                for (int j = 2; j <= n; j++)
+                    for (int i = 1; i <= n - j + 1; i++)
+                        foreach (Rule r in rules)
+                            if (r.right.Length == 2)
+                                if (!IsIn(r.left, table[i, j]))
+                                    if (CanGen(j, i, r))
+                                    {
+                                        table[i, j] += r.left;
+                                        // Createbutton(90, 90, w, table);
+                                    }
+            }
+            /// <summary>
+            /// run CYK :3
+            /// </summary>
+            /// <param name="x"></param>
+            public void Process(string x)
+            {
+                InitData(x);
+                FirstProcess();
+                GenTable();
+            }
+
+            //hiện thị ra ký tự test các bước
+            public string[,] PrintResult()
+            {
+                for (int i = 1; i <= n; i++)
+                {
+                    table[0, i] = i.ToString();
+                    table[i, 0] = w[i - 1].ToString();
+                }
+
+                return table;
+
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //lbl_list.Text = a;
+            button1.Enabled = false;
+        }
+
+        string[,] table;
+        int kk = 0;
+
+        private void btn_test_Click(object sender, EventArgs e)
+        {
+            PTCYK parser = new PTCYK();
+
+            List<Rule> rules = new List<Rule>();
+
+            // nạp bộ luật vào bộ phân tích
+            parser.AddRule('S', "AB");
+            parser.AddRule('S', "XB");
+            parser.AddRule('T', "AB");
+            parser.AddRule('T', "XB");
+            parser.AddRule('X', "AT");
+            parser.AddRule('A', "a");
+            parser.AddRule('B', "b");
+            rules = parser.PrintAllRules();
+
+            //gán bộ luật vào lbllist
+            string a = "";
+            foreach (Rule r in rules)
+            {
+                a += r.ToFineString() + "\n";
+            }
+
+            lbl_list.Text = a;
+            //
+            //thực hiện gán giá trị vào button và show ra pnl_Main
+            parser.Process(txt_input.Text.ToString());
+            table = parser.PrintResult();
+            timer.Start();
+            //kiểm tra nếu đã chạy hết thì dừng timer, xóa text nhập và enable btnRefresh
+            if (kk == txt_input.Text.Length+1)
+            {
+                timer.Stop();
+                button1.Enabled = true;
+                MessageBox.Show("CYK done!","Infomation");
+                txt_input.Text = "";
+                kk = 0;
+            }
+            
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            timer.Stop();
+            //MessageBox.Show(kk.ToString());
+            //Createbutton(60, 50, txt_input.Text, table, kk);
+            Createbutton(Convert.ToInt32(txtW_btn.Text), Convert.ToInt32(txtH_btn.Text), txt_input.Text, table, kk);
+            kk++;
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pnl_Main.Controls.Clear();//remove all controls
+            button1.Enabled = false;
+            ////remove controls is button
+            //foreach (Control item in pnl_Main.Controls.OfType<Button>())
+            //{
+            //    pnl_Main.Controls.Remove(item);
+            //}
+            //button1.Enabled = false;
+        }
+    }
+}
